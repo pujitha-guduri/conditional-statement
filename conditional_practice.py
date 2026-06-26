861c1918394cef0f3f568e610350a77196582b0d
pujitha <pujithaguduri1908@gmail.com>
2026-06-26 15:17:45 +0530

    Added conditional statements assignment

---
+++ conditional_practice.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("Eligible for Vote")
+    print("Eligible for Driving License")
+else:
+    print("Not Eligible for Vote")
+    print("Not Eligible for Driving License")
